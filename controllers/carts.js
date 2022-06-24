34eb1fe18d3074fdc1cdcf739f3d2a4113f689c8
matt pero <mattpero22@gmail.com>
2022-06-24 02:11:18 -0400

    feat: cart show, create, and get user cart route

--- controllers/carts.js
+++ controllers/carts.js
@@ -4,19 +4,28 @@ const Cart = require('../models/cart.js');
 const router = express.Router();
 
 // ROUTES
-// Show
-router.get('/:uid', async (req, res) => {
+// Index
+router.get('/', async (req, res) => {
     try {
-        res.json(await Cart.find({UID: uid}));
+        res.json(await Cart.find());
     } catch (error) {
         res.status(400).json(error);
     }
 });
 
+// Show User Cart
+router.get('/:uid', async(req,res) => {
+    try {
+        res.json(await Cart.find({UID: req.params.uid}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+
 // Create
 router.post('/', async (req, res) => {
     try {
-        res.json(await User.create(req.body))
+        res.json(await Cart.create(req.body))
     } catch (error) {
         res.status(400).json(error);
     }
